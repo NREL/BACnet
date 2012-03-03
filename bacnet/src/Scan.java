08782fa6347196f4ab3aca5b64a9d3097619c51c
Jason Turner <lefticus>
2012-03-03 21:13:28 +0000

    Merge ssh://rsbld1.nrel.gov:3389/home/jturner2/bacnet-git

--- bacnet/src/Scan.java
+++ bacnet/src/Scan.java
@@ -20,14 +20,16 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
  */
-package com.serotonin.bacnet4j.test;
+package gov.nrel.bacnet;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
-
+import java.util.concurrent.TimeUnit;
+import java.util.HashSet;
+import java.util.logging.*;
 import com.serotonin.bacnet4j.service.confirmed.ReinitializeDeviceRequest.ReinitializedStateOfDevice;
 import com.serotonin.bacnet4j.LocalDevice;
 import com.serotonin.bacnet4j.RemoteDevice;
@@ -54,51 +56,123 @@ import com.serotonin.bacnet4j.type.enumerated.EventType;
 import com.serotonin.bacnet4j.type.enumerated.MessagePriority;
 import com.serotonin.bacnet4j.type.enumerated.NotifyType;
 import com.serotonin.bacnet4j.type.enumerated.PropertyIdentifier;
+import com.serotonin.bacnet4j.type.enumerated.Segmentation;
 import com.serotonin.bacnet4j.type.notificationParameters.NotificationParameters;
 import com.serotonin.bacnet4j.type.primitive.Boolean;
 import com.serotonin.bacnet4j.type.primitive.CharacterString;
 import com.serotonin.bacnet4j.type.primitive.ObjectIdentifier;
 import com.serotonin.bacnet4j.type.primitive.UnsignedInteger;
+import com.serotonin.bacnet4j.type.primitive.Unsigned16;
 import com.serotonin.bacnet4j.util.PropertyReferences;
 import com.serotonin.bacnet4j.util.PropertyValues;
 
 
-class MyExceptionListener extends DefaultExceptionListener {  
-  @Override  
-    public void unimplementedVendorService(UnsignedInteger vendorId, UnsignedInteger serviceNumber, ByteQueue queue) 
-    {  
-      if (vendorId.intValue() == 8 && serviceNumber.intValue() == 1) {  
-        // do nothing  
-      }  
-      else  
-        super.unimplementedVendorService(vendorId, serviceNumber, queue);  
-    }  
-}  
-
-/**
- * @author Matthew Lohbihler
- */
 public class Scan {
-  public static void main(String[] args) throws Exception {
-    run();
+  class MyExceptionListener extends DefaultExceptionListener {  
+    @Override  
+      public void unimplementedVendorService(UnsignedInteger vendorId, UnsignedInteger serviceNumber, 
+          ByteQueue queue) 
+      {  
+        if (vendorId.intValue() == 8 && serviceNumber.intValue() == 1) {  
+          // do nothing  
+        }  
+        else  
+          super.unimplementedVendorService(vendorId, serviceNumber, queue);  
+      }  
+  }  
 
-    Thread.sleep(30000);
+  static class Handler extends DefaultDeviceEventListener {
+    private BlockingQueue<RemoteDevice> devices = new LinkedBlockingQueue<RemoteDevice>();
+    private HashSet<RemoteDevice> founddevices = new HashSet<RemoteDevice>();
+
+    @Override
+      public void iAmReceived(RemoteDevice d) {
+        System.out.println("RemoteDevice found: " + d);
+
+        try {
+          if (!founddevices.contains(d))
+          {
+            founddevices.add(d);
+            devices.put(d);
+          } else {
+            System.out.println("Device already queued for scanning: " + d);
+          }
+        } catch (java.lang.InterruptedException e) {
+        }
+      }
+
+    public BlockingQueue<RemoteDevice> remoteDevices()
+    {
+      return devices;
+    }
   }
-  private static void printObject(ObjectIdentifier oid, PropertyValues pvs) {
+
+
+  private Logger logger;
+
+  public static void main(String[] args) throws Exception {
+    Scan s = new Scan();
+    s.run();
+  }
+
+  private void printObject(ObjectIdentifier parentoid, ObjectIdentifier oid, PropertyValues pvs,
+      java.io.PrintWriter writer) {
+
+    try {
+      Encodable parentObjectName = pvs.get(parentoid, PropertyIdentifier.objectName);
+      Encodable objectName = pvs.get(oid, PropertyIdentifier.objectName);
+
+      Encodable presentValue = null;
+      Encodable units = null;
+
+      try {
+        presentValue = pvs.get(oid, PropertyIdentifier.presentValue);
+        units = pvs.get(oid, PropertyIdentifier.units);
+      } catch (Exception e) {
+      }
+
+
+      writer.println(String.format("%s, %s, %s, %s, %s, %s, %s", 
+            parentoid,
+            parentObjectName,
+            oid.getObjectType(),
+            oid.getInstanceNumber(),
+            objectName,
+            presentValue,
+            units));
+    } catch (Exception e) {
+    }
+
+
     System.out.println(String.format("\t%s", oid));
     for (ObjectPropertyReference opr : pvs) {
       if (oid.equals(opr.getObjectIdentifier())) {
-        System.out.println(String.format("\t\t%s = %s", opr.getPropertyIdentifier().toString(), pvs
-              .getNoErrorCheck(opr)));
+        System.out.println(String.format("\t\t%s = %s", opr.getPropertyIdentifier().toString(), 
+            pvs.getNoErrorCheck(opr)));
       }
 
     }
   }
 
 
-  private static void run() {
+  private void run() {
+    logger = Logger.getLogger("BACnetScanner");
+
+    try {
+      FileHandler fh = new FileHandler("LogFile.log", true);
+      logger.addHandler(fh);
+      SimpleFormatter formatter = new SimpleFormatter();
+      fh.setFormatter(formatter);
+    } catch (Exception e) {
+      logger.log(Level.SEVERE, "Unable to create log file", e);
+    }
+
+    logger.setLevel(Level.ALL);
+
     LocalDevice localDevice = new LocalDevice(1234, "192.168.2.255");
     localDevice.setPort(LocalDevice.DEFAULT_PORT);
+    localDevice.setTimeout(localDevice.getTimeout() * 3);
+    localDevice.setSegTimeout(localDevice.getSegTimeout() * 3);
     Handler h = new Handler();
     localDevice.getEventHandler().addListener(h);
     localDevice.setExceptionListener(new MyExceptionListener());
@@ -110,6 +184,7 @@ public class Scan {
       return;
     }
 
+
     byte[] ip = new byte[4];
     ip[0] = (byte) 192; // A
     ip[1] = (byte) 168; // B
@@ -119,22 +194,20 @@ public class Scan {
     WhoIsRequest whois = new WhoIsRequest();
     while (true) {
       String ipstr = IpAddressUtils.toIpString(ip);
-      // System.out.println(ipstr);
       localDevice.setBroadcastAddress(ipstr);
-      System.out.println(ipstr);
+      logger.fine("Scanning IP: " + ipstr);
       try {
         localDevice.sendBroadcast(whois);
       }
       catch (BACnetException e) {
-        System.out.println("Exception at " + ipstr);
-        e.printStackTrace();
+        logger.log(Level.WARNING, "Exception occured with ip " + ipstr, e);
       }
 
       try {
         increment(ip);
       }
       catch (Exception e) {
-        System.out.println("Done");
+        logger.info("Done pinging devices");
         break;
       }
     }
@@ -142,16 +215,37 @@ public class Scan {
     BlockingQueue<RemoteDevice> remoteDevices = h.remoteDevices();
 
 
+    java.io.FileOutputStream file = null;
+    java.io.PrintWriter w = null;
+    try {
+      file = new java.io.FileOutputStream("output.csv");
+    } catch (Exception e) {
+      logger.log(Level.SEVERE, "Error opening output file", e);
+    }
+
+    if (file != null)
+    {
+      w = new java.io.PrintWriter(file, true);
+    }
+
+    w.println("Device OID, Device Name, Object Type, Object Instance Number, Object Name, Object Value");
+
     while (true)
     {
       try {
-        RemoteDevice rd = remoteDevices.take();
-        System.out.println("Getting device info: " + rd);
+        RemoteDevice rd = remoteDevices.poll(300, TimeUnit.SECONDS);
+        if (rd == null)
+        {
+          break;
+        }
+        logger.info("Getting device info: " + rd);
         localDevice.getExtendedDeviceInformation(rd);
 
+        @SuppressWarnings("unchecked")
         List<ObjectIdentifier> oids = ((SequenceOf<ObjectIdentifier>) localDevice.sendReadPropertyAllowNull(rd, rd.getObjectIdentifier(), PropertyIdentifier.objectList)).getValues();
 
-        System.out.println("Oids found: " + Integer.toString(oids.size()));
+        logger.info("Getting device info: " + rd);
+        logger.fine("Oids found: " + Integer.toString(oids.size()));
         PropertyReferences refs = new PropertyReferences();
         // add the property references of the "device object" to the list
         refs.add(rd.getObjectIdentifier(), PropertyIdentifier.objectName);
@@ -160,6 +254,7 @@ public class Scan {
         for (ObjectIdentifier oid : oids) {
           refs.add(oid, PropertyIdentifier.objectName);
           refs.add(oid, PropertyIdentifier.presentValue);
+          refs.add(oid, PropertyIdentifier.units);
 //          refs.add(oid, PropertyIdentifier.maximumValue);
 //          refs.add(oid, PropertyIdentifier.minimumValue);
 //          refs.add(oid, PropertyIdentifier.averageValue);
@@ -167,25 +262,47 @@ public class Scan {
 //          refs.add(oid, PropertyIdentifier.minimumValueTimestamp);
         }
 
-        System.out.println("Start read properties");
+        logger.fine("Start read properties");
         final long start = System.currentTimeMillis();
 
-        PropertyValues pvs = localDevice.readProperties(rd, refs);
-        //PropertyValues pvs = localDevice.readPresentValues(rd);
+        logger.fine(String.format("Trying to read %d properties", refs.size()));
 
-        System.out.println(String.format("Properties read done in %d ms", System.currentTimeMillis() - start));
-        printObject(rd.getObjectIdentifier(), pvs);
-        for (ObjectIdentifier oid : oids) {
-          printObject(oid, pvs);
+      
+        PropertyValues pvs; 
+        try {
+          pvs = localDevice.readProperties(rd, refs);
+          logger.fine(String.format("Properties read done in %d ms", System.currentTimeMillis() - start));
+        } catch (BACnetException e) {
+          // let's try breaking it up ourselves if it fails
+          List<PropertyReferences> lprs = refs.getPropertiesPartitioned(1);
+
+          pvs = new PropertyValues();
+          for (PropertyReferences prs: lprs)
+          {
+            PropertyValues lpvs = localDevice.readProperties(rd, prs);
+
+            for (ObjectPropertyReference opr : lpvs)
+            {
+              pvs.add(opr.getObjectIdentifier(), opr.getPropertyIdentifier(), opr.getPropertyArrayIndex(), 
+                  lpvs.getNoErrorCheck(opr));
+            }
+          }
         }
+
+        printObject(rd.getObjectIdentifier(), rd.getObjectIdentifier(), pvs, w);
+        for (ObjectIdentifier oid : oids) {
+          printObject(rd.getObjectIdentifier(), oid, pvs, w);
+        }
+
       } catch (BACnetException e) {
-        System.out.println("Exception at " + e);
-        e.printStackTrace();
+        logger.log(Level.SEVERE, "BACnetException", e);
       } catch (java.lang.InterruptedException e) {
+        logger.info("Done Scanning Devices");
         break;
       }
     }
 
+    localDevice.terminate();
   }
 
   private static void increment(byte[] ip) throws Exception {
@@ -223,22 +340,4 @@ public class Scan {
     }
   }
 
-  static class Handler extends DefaultDeviceEventListener {
-    private BlockingQueue<RemoteDevice> devices = new LinkedBlockingQueue<RemoteDevice>();
-
-    @Override
-      public void iAmReceived(RemoteDevice d) {
-        System.out.println("RemoteDevice found: " + d);
-
-        try {
-          devices.put(d);
-        } catch (java.lang.InterruptedException e) {
-        }
-      }
-
-    public BlockingQueue<RemoteDevice> remoteDevices()
-    {
-      return devices;
-    }
-  }
 }
