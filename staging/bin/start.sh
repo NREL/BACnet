8da55d48a376c3e823b0acd485961f15aa59b44c
Nicholas Long <nicholas.long@nrel.gov>
2013-02-01 07:40:17 -0700

    forcing log directory to exist, making sh files executable

--- staging/bin/start.sh
+++ staging/bin/start.sh
@@ -4,6 +4,6 @@
 
 nohup java -cp "../lib/*" gov.nrel.consumer.Main 2>&1 >../logs/out.log
 
-####./runscanner.sh -f ../conf/outputFilter.json -vv -dev eth4 -s -i 65001 -u https://databus.nrel.gov:5502 -r register:10768272987:b1:4814227944682770547 -g bacnet > ../logs/output.log 2>&1 &
+##./runscanner.sh -f ../conf/filter.json -vv -dev eth4 -s -i 65001 -u https://databus.nrel.gov:5502 -r register:10768272987:b1:4814227944682770547 -g bacnet > ../logs/output.log 2>&1 &
 
 
