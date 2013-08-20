e7ac5713fe8b89ae11eb1f135318ff5c7922f19f
Nicholas Long <nicholas.long@nrel.gov>
2013-08-20 13:33:54 -0600

    adding ruby submodules and role

--- vagrant/chef/roles/base.rb
+++ vagrant/chef/roles/base.rb
@@ -7,5 +7,36 @@ run_list([
              "recipe[git]",
              "recipe[java]",
              "recipe[gradle::tarball]",
-             "recipe[vim]"
+             "recipe[vim]",
+             "recipe[ruby_build]",
+             "recipe[rbenv::system]",
          ])
+
+default_attributes({
+       :rbenv => {
+           :upgrade => true,
+           :rubies => [
+               {
+                   :name => '2.0.0-p195',
+                   :environment => {
+                       'RUBY_CONFIGURE_OPTS' => '--enable-shared', # needs to be set for openstudio linking
+                       'CONFIGURE_OPTS' => '--disable-install-doc'
+                   }
+               },
+               {
+                   :name => 'jruby-1.7.4',
+               }
+           ],
+           :no_rdoc_ri => true,
+           :global => "2.0.0-p195",
+           :gems => {
+               "2.0.0-p195" => [
+                   {
+                       :name => "rubygems-bundler",
+                       :version => "1.2.2",
+                   },
+               ]
+           }
+       }
+   }
+)
